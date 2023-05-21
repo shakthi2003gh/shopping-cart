b0372c2499aa4a248b0ee2ec4526db2f55c71c20
shakthi <shakthikumar123456789@gmail.com>
2023-05-21 10:46:49 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
