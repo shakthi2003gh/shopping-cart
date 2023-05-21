8d14f6b3fc78c1ada7950e3cdb0e4a17b55c04d6
shakthi <shakthikumar123456789@gmail.com>
2023-05-21 17:32:18 +0530

    Create home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import ProductCard from "../components/card";
+import { StateContext } from "../context/state";
 
 export default function Home() {
-  return <div>home</div>;
+  const { products, myFavProducts, onLike } = useContext(StateContext);
+
+  return (
+    <div className="home">
+      <div className="products">
+        {products.map(({ id, ...rest }) => (
+          <ProductCard
+            key={id}
+            id={id}
+            liked={myFavProducts.includes(id)}
+            onLike={onLike}
+            {...rest}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
