8cca4b12bb8b81917968811587d01265bc0f092b
shakthi <shakthikumar123456789@gmail.com>
2023-05-23 17:13:10 +0530

    Make Addtocart button functional

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,16 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
 import {
   faCartShopping,
+  faMinus,
+  faPlus,
   faStar as fullStar,
 } from "@fortawesome/free-solid-svg-icons";
+import { StateContext } from "../context/state";
 import { getProduct } from "../services";
 import Button from "../components/button";
 
 export default function Product() {
   const { id } = useParams();
+  const { cartProducts, onAddToCart, onRemoveOneFromCart } =
+    useContext(StateContext);
   const [product, setProduct] = useState({});
 
   const rating = product?.rating?.rate || 0;
@@ -18,6 +23,14 @@ export default function Product() {
   useEffect(() => {
     getProduct(id).then((d) => setProduct(d));
   }, []);
+
+  const handleAddToCart = () => {
+    onAddToCart(Number(id));
+  };
+
+  const handleRemoveOneFromCart = () => {
+    onRemoveOneFromCart(Number(id));
+  };
 
   if (Object.keys(product).length)
     return (
@@ -40,7 +53,27 @@ export default function Product() {
             <div className="price">${product.price}</div>
 
             <div className="buttons">
-              <Button label={"Add to cart"} icon={faCartShopping} />
+              {cartProducts[id] ? (
+                <div className="quantity-btn">
+                  <FontAwesomeIcon
+                    className="icon"
+                    icon={faMinus}
+                    onClick={handleRemoveOneFromCart}
+                  />
+                  <span>{cartProducts[id]}</span>
+                  <FontAwesomeIcon
+                    className="icon"
+                    icon={faPlus}
+                    onClick={handleAddToCart}
+                  />
+                </div>
+              ) : (
+                <Button
+                  label={"Add to cart"}
+                  icon={faCartShopping}
+                  onClick={handleAddToCart}
+                />
+              )}
               <Button label={"Buy now"} className="btn--primary" />
             </div>
           </div>
