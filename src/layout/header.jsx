9352509f0b23966ec55ede08bb7172b288680d36
shakthi <shakthikumar123456789@gmail.com>
2023-05-23 17:11:42 +0530

    Add cart navigation button

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { StateContext } from "../context/state";
 
 export default function Header() {
+  const { cartProducts } = useContext(StateContext);
+  const isSomethingInCart = !!Object.keys(cartProducts).length;
+
   return (
     <header>
       <div className="container">
         <Link to="/">Shopping App</Link>
+
+        <NavLink
+          to="/cart"
+          className={"icon" + (isSomethingInCart ? " item-in-cart" : "")}
+        >
+          <FontAwesomeIcon icon={faCartShopping} />
+        </NavLink>
       </div>
     </header>
   );
